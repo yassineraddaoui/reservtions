045cb903da0a122480b6548fede59fc82e5752a6
yassine raddaoui <yassineraddaouicr7@gmail.com>
2024-03-03 22:58:25 +0100

    database constraint for date of booking

--- models/booking.js
+++ models/booking.js
@@ -4,7 +4,16 @@ const bookingSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   room: { type: mongoose.Schema.Types.ObjectId, ref: 'Room', required: true },
   dates: { type: [Date], required: true },
-  bookingDate: { type: Date, required: true },
+  bookingDate: { 
+    type: Date, 
+    required: true,
+    validate: {
+      validator: function(value) {
+        return value > new Date();
+      },
+      message: 'Booking date must be after the current date'
+    }
+  }, 
   confirmCode : { type:String,required:false},
   canceled : { type:Boolean,required:true,default:false},
 });
